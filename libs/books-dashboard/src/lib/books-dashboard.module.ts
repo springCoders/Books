6a9eb93a0da95bb57e883f589e17f001b2937846
Anupam Agnihotri <xanukum@gmail.com>
2019-03-07 12:09:58 +0530

    design and develop

--- libs/books-dashboard/src/lib/books-dashboard.module.ts
+++ libs/books-dashboard/src/lib/books-dashboard.module.ts
@@ -8,6 +8,8 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import {MatTabsModule} from '@angular/material/tabs';
 import {LayoutModule} from '@angular/cdk/layout';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -21,7 +23,7 @@ import {LayoutModule} from '@angular/cdk/layout';
       { path: '', pathMatch: 'full', component: HomeComponent }
     ])
   ],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
   exports: [HomeComponent]
 })
 export class BooksDashboardModule {}
